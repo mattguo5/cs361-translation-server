b2e13063b688491bb1a0fd18bf90d96ce1d888ab
mattguo5 <guom@oregonstate.edu>
2021-05-22 18:52:29 -0700

    this time cors fix?

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import cors from 'cors'
+var cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -8,6 +8,25 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const port = process.env.PORT || 3001;
 
